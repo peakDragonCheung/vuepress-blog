be9fcb9ae5549d120358b5a06232ddcadd468b57
张龙峰 <1241863562@qq.com>
2021-04-15 22:19:03 +0800

    提交可以使用3000端口进行访问页面的端口。

--- publishEnd/app.js
+++ publishEnd/app.js
@@ -1,15 +1,24 @@
 const Koa = require('koa');
 const cors = require('@koa/cors');
+var fs = require('fs');
 const { publish } = require('./publish')
 const app = new Koa();
 app.use(cors());
 app.use(async ctx => {
+  if(ctx.path === '/') {
+    ctx.res.setHeader('Content-type','text/html;charset=utf-8');
+    const result = fs.readFileSync('./publishEnd/publish.html','utf8');
+    ctx.status = 200;
+    ctx.res.end(result);
+  }
   if(ctx.path === '/publish') {
     const result = await publish();
     ctx.body = result;
-  } else {
-    ctx.body = '大佬能不能别连我的服务器，我是一个弟弟啊'
   }
+  if(ctx.path !== '/' || ctx.path !== '/publish') {
+     ctx.body = '大佬别连我的服务器啊。。。。。。。。。';
+  }
+  
 });
 
 console.log('开始监听......');
